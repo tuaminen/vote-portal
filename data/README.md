2c31121a2abe554bf089e1b2c701b3bd88115963
Petri Tuominen <petri.tuominen@remion.com>
2025-09-15 02:30:09 +0300

    Use JPG in images

---
+++ data/README.md
@@ -0,0 +1,6 @@
+
+
+PNG to jpg:
+
+mogrify -format jpg -quality 85 -resize 75%   *.png
+
--- data/upload_items.sh
+++ data/upload_items.sh
@@ -7,23 +7,23 @@ HOST="http://localhost:8080"
 curl -fsS -X DELETE "$HOST/items" || true
 
 post() {
-  # usage: post "description text" image.png
+  # usage: post "description text" image.jpg
   curl -fsS -X POST "$HOST/items" \
     --form-string "description=$1" \
     -F "image=@$2"
 }
 
-post 'Lautapeli at toimisto/Lategame/Taverna' 'lautapeli.png'
-post 'Save file -visiitti https://www.savefile.fi' 'savefile.png'
-post 'Hervanta tour (ratikalla Hervantaan, Kultainen apina jne.)' 'herwood.png'
-post 'Pispala tour (Pulteri, Vastavirta, Kujakolli)' 'pispala.png'
-post 'ZBase (arcade, baaritiski) https://zbase.fi/tampere/' 'zbase.png'
-# post 'Counter Strike' 'counterstrike.png'
-post 'Ikuri Arcade https://www.ikuriarcade.com/' 'ikuriarcade.png'
-post 'Pinball Union https://pinballunion.fi/' 'pinball_union.png'
-post 'Pubivisailua https://pubivisat.fi/tampere' 'pub_quiz.png'
-post 'Pubikierros (Kahdet kasvot,Ohranjyvä,..)' 'pubcrawl.png'
-post 'Karaoke' 'karaoke.png'
-post 'LANit / peli-ilta toimistolla' 'gaming.png'
-post 'Sivuluisua https://www.icerange.fi/' 'icerange.png'
-post 'Space Bowling https://www.spacebowling.fi/tampere/' 'bowling.png'
+post 'Lautapeli at toimisto/Lategame/Taverna' 'lautapeli.jpg'
+post 'Save file -visiitti https://www.savefile.fi' 'savefile.jpg'
+post 'Hervanta tour (ratikalla Hervantaan, Kultainen apina jne.)' 'herwood.jpg'
+post 'Pispala tour (Pulteri, Vastavirta, Kujakolli)' 'pispala.jpg'
+post 'ZBase (arcade, baaritiski) https://zbase.fi/tampere/' 'zbase.jpg'
+# post 'Counter Strike' 'counterstrike.jpg'
+post 'Ikuri Arcade https://www.ikuriarcade.com/' 'ikuriarcade.jpg'
+post 'Pinball Union https://pinballunion.fi/' 'pinball_union.jpg'
+post 'Pubivisailua https://pubivisat.fi/tampere' 'pub_quiz.jpg'
+post 'Pubikierros (Kahdet kasvot,Ohranjyvä,..)' 'pubcrawl.jpg'
+post 'Karaoke' 'karaoke.jpg'
+post 'LANit / peli-ilta toimistolla' 'gaming.jpg'
+post 'Sivuluisua https://www.icerange.fi/' 'icerange.jpg'
+post 'Space Bowling https://www.spacebowling.fi/tampere/' 'bowling.jpg'
